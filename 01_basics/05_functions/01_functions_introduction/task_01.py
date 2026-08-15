02ecf94c7f35978ac006667928c1a63a3daf6d06
Ashot Elizbaryan <ashot.elizbaryan@gmail.com>
2026-08-15 21:19:41 +0400

    Add first function example
    Add 01_basics/05_functions/01_functions_introduction/task_01.py defining show_text() which prints "It's my first function" and invokes it as a demonstration of a no-parameter function.

---
+++ 01_basics/05_functions/01_functions_introduction/task_01.py
@@ -0,0 +1,19 @@
+# Input:
+#
+# Declare a function with no parameters that prints:
+#
+# It's my first function
+#
+# After declaring the function, call it.
+#
+# Input:
+# No input
+#
+# Output:
+# It's my first function
+
+def show_text():
+    print("It's my first function")
+
+
+show_text()
